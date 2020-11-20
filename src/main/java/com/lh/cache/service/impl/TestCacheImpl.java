f63bdf5e99e78d00ce884cbf86f109e96612d70a
haol <haol@jumei.com>
2020-11-20 14:41:18 +0800

    fix

--- src/main/java/com/lh/cache/service/impl/TestCacheImpl.java
+++ src/main/java/com/lh/cache/service/impl/TestCacheImpl.java
@@ -2,6 +2,8 @@ package com.lh.cache.service.impl;
 
 import com.lh.cache.dto.Message;
 import com.lh.cache.service.TestCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -16,32 +18,34 @@ import org.springframework.stereotype.Service;
 @Service("testCache")
 public class TestCacheImpl implements TestCache {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestCacheImpl.class);
+
 
     @Override
     @CachePut(value = "message", key = "#message.id")
     public Message add(Message message) {
-        System.out.println("add========================" + message);
+        logger.info("add========================{}", message);
         return message;
     }
 
     @Override
     @Cacheable(value = "message", key = "#id")
     public Message get(int id) {
-        System.out.println("get========================" + id);
+        logger.info("get========================{}", id);
         return new Message(1, "测试ehcache缓存");
     }
 
     @Override
     @CachePut(value = "message", key = "#message.id")
     public Message update(Message message) {
-        System.out.println("update========================" + message);
+        logger.info("update========================{}", message);
         return message;
     }
 
     @Override
     @CacheEvict(value = "message", key = "#id")
     public boolean delete(int id) {
-        System.out.println("delete========================" + id);
+        logger.info("delete========================{}", id);
         return false;
     }
 }
